47c62b801c40c2734890f5948a2adf04734dc995
Oconer2 <ixsplinter2@gmail.com>
2024-10-23 22:39:12 -0600

    visualizacion pdf sin descarga

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,15 +2,17 @@
 import { ref, onMounted, computed } from "vue";
 import axios from 'axios';
 import { Link, usePage } from "@inertiajs/vue3";
-import { ChevronLeft, Search, ChevronDown, ChevronUp, FileText, LogIn } from 'lucide-vue-next';
+import { ChevronLeft, Search, ChevronDown, ChevronUp, FileText, LogIn,XCircle} from 'lucide-vue-next';
 
-const principales = ref( [] );
-const searchQuery = ref( "" );
-const searchResult = ref( [] );
-const loading = ref( true );
-const error = ref( null );
-const sidebarOpen = ref( true );
-const results = ref( null );
+const principales = ref([]);
+const searchQuery = ref("");
+const searchResult = ref([]);
+const loading = ref(true);
+const error = ref(null);
+const sidebarOpen = ref(true);
+const results = ref(null);
+const pdfUrl = ref(null); // URL del PDF a visualizar
+const pdfVisible = ref(false); // Controla si la vista del PDF está activa
 
 // Función para obtener categorías y archivos
 const fetchCategoriesWithFiles = async () => {
@@ -58,27 +60,34 @@ const isPdf = ( file ) => {
     return file.nombre_archivo.toLowerCase().endsWith( '.pdf' );
 };
 
+/*/////////////////////////////////////////////////////////////// */
 // Función para previsualizar archivos PDF
-const previewPdf = async ( id ) => {
+const previewPdf = async (id) => {
     try {
-        const response = await axios.get( `/files/${ id }/preview`, {
-            headers: {
-                'Accept': 'application/json'
-            },
+        const response = await axios.get(`/files/${id}/preview`, {
+            headers: { 'Accept': 'application/json' },
             withCredentials: true
-        } );
-        const fileUrl = response.data.url;
-        window.open( fileUrl, '_blank' );
-    } catch ( err ) {
-        console.error( "Error al previsualizar el archivo:", err );
+        });
+        pdfUrl.value = response.data.url;
+        pdfVisible.value = true; // Activar vista del PDF
+    } catch (err) {
+        console.error("Error al previsualizar el archivo:", err);
     }
 };
+
+// Función para cerrar la vista del PDF
+const closePdfView = () => {
+    pdfVisible.value = false;
+    pdfUrl.value = null; // Limpiar la URL del PDF
+};
+
+/*/////////////////////////////////////////////////////////////// */ 
 
 // Función para alternar el sidebar
 const toggleSidebar = () => {
     sidebarOpen.value = !sidebarOpen.value;
 };
-
+ 
 // Computed property para obtener archivos aplanados
 const flattenedFiles = computed( () => {
     const files = [];
@@ -107,8 +116,8 @@ const flattenedFiles = computed( () => {
     <div class="flex flex-col h-screen overflow-hidden font-sans bg-gray-100">
         <header class="sticky top-0 flex justify-between items-center p-4 bg-white shadow-md z-50">
             <div class="flex items-center">
-                <img src="/img/logo-letters.png" alt="Logo" class="w-10 h-10 mr-4">
-                <h1 class="text-2xl font-bold text-gray-800">Biblioteca Pública</h1>
+                <a href="/" > <img src="/img/logo-letters.png" alt="Logo" class="w-10 h-10 mr-4"></a>
+                <h1>Biblioteca Publica</h1>
             </div>
             <Link :href=" route( 'login' ) "
                 class="flex items-center px-4 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
@@ -118,54 +127,37 @@ const flattenedFiles = computed( () => {
         </header>
 
         <div class="flex flex-1 overflow-hidden">
-            <aside
-                :class=" [ 'w-80 bg-white shadow-lg transition-all duration-300 ease-in-out', sidebarOpen ? 'translate-x-0' : '-translate-x-full' ] ">
+            <aside :class=" [ 'w-80 bg-white shadow-lg transition-all duration-300 ease-in-out', sidebarOpen ? 'translate-x-0' : '-translate-x-full' ] ">
                 <div class="h-full overflow-y-auto overflow-x-auto">
                     <div class="p-4">
                         <h2 class="text-lg font-bold mb-4 text-gray-800">Documentos públicos</h2>
-                        <div v-for="(    category, categoryIndex) in searchResult" :key=" categoryIndex " class="mb-2">
-                            <button @click="toggleCategory( category )" :class=" [
-                                'flex justify-between items-center w-full p-2 text-left rounded transition-colors duration-200',
-                                category.expanded ? 'bg-gray-200' : 'hover:bg-gray-100'
-                            ] ">
+                        <div v-for="(category, index) in searchResult" :key="index" class="mb-2">
+                        <button @click="toggleCategory(category)" class="flex justify-between items-center w-full p-2 text-left rounded transition-colors duration-200 hover:bg-gray-100">
                                 <div class="flex items-center">
                                     <img src="/img/folder.svg" alt="folder" class="w-5 h-5 mr-2" />
-                                    <span :class=" [
-                                        'text-gray-700 bold',
-                                        category.expanded ? 'font-semibold' : 'font-normal'
-                                    ] ">
-                                        {{ category.nombre_categoria }}
-                                    </span>
+                                    <span class="text-gray-700">{{ category.nombre_categoria }}</span>
                                 </div>
-                                <ChevronDown v-if=" !category.expanded " class="w-5 h-5 text-gray-500" />
-                                <ChevronUp v-else class="w-5 h-5 text-gray-500" />
-                            </button>
+                                <ChevronDown v-if="!category.expanded" class="w-5 h-5 text-gray-500" />
+                            <ChevronUp v-else class="w-5 h-5 text-gray-500" />
+                        </button>
 
-                            <div v-if=" category.expanded " class="ml-4 mt-2">
-                                <div v-for="(    subcategory, subIndex) in category.subcategorias" :key=" subIndex "
-                                    class="mb-2">
-                                    <button @click="toggleSubcategory( subcategory )" :class=" [
-                                        'flex justify-between items-center w-full p-2 text-left rounded transition-colors duration-200',
-                                        subcategory.expanded ? 'bg-gray-200' : 'hover:bg-gray-100'
-                                    ] ">
+                        <div v-if="category.expanded" class="ml-4 mt-2">
+                            <div v-for="(subcategory, subIndex) in category.subcategorias" :key="subIndex" class="mb-2">
+                                <button @click="toggleSubcategory(subcategory)" class="flex justify-between items-center w-full p-2 text-left rounded transition-colors duration-200 hover:bg-gray-100">
                                         <div class="flex items-center">
                                             <img src="/img/corner-down-right.svg" alt="corner-down-right"
                                                 class="w-4 h-4 mr-2" />
-                                            <span class="text-gray-600">{{ subcategory.nombre_categoria }}</span>
+                                                <span class="text-gray-600">{{ subcategory.nombre_categoria }}</span>
                                         </div>
-                                        <ChevronDown v-if=" !subcategory.expanded " class="w-4 h-4 text-gray-400" />
-                                        <ChevronUp v-else class="w-4 h-4 text-gray-400" />
-                                    </button>
-                                    <ul v-if=" subcategory.expanded " class="ml-4 space-y-1 mt-2">
-                                        <li v-for="    file in subcategory.files    " :key=" file.id " class="py-1">
-                                            <a :href=" file.ubicacion_archivo "
-                                                class="flex items-center text-blue-600 hover:text-blue-800 hover:underline transition-colors duration-200">
-                                                <FileText class="w-4 h-4 mr-2" />
-                                                <span class="truncate">{{ file.nombre_archivo }}</span>
-                                            </a>
-                                            <button v-if=" isPdf( file ) " @click="previewPdf( file.id )"
-                                                class="ml-2 text-sm text-blue-500 hover:underline transition-colors duration-200">Previsualizar
-                                                PDF</button>
+                                        <ChevronDown v-if="!subcategory.expanded" class="w-4 h-4 text-gray-400" />
+                                    <ChevronUp v-else class="w-4 h-4 text-gray-400" />
+                                </button>
+                                <ul v-if="subcategory.expanded" class="ml-4 mt-2">
+                                    <li v-for="file in subcategory.files" :key="file.id">
+                                            <button @click.prevent="previewPdf(file.id)" class="text-blue-500 flex items-center">
+                                            <FileText class="w-4 h-4 mr-2" />
+                                            {{ file.nombre_archivo }}
+                                        </button>
                                         </li>
                                     </ul>
                                 </div>
@@ -182,16 +174,14 @@ const flattenedFiles = computed( () => {
                             class="flex  bg-white rounded-full shadow-lg overflow-hidden transition-all duration-300 hover:shadow-xl">
                             <input type="text" v-model=" searchQuery " placeholder="Buscar archivos por nombre"
                                 class="flex-1 pl-12 pr-4 py-4 text-gray-700 border-transparent placeholder-gray-400 bg-transparent focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50" />
-                            <button @click=" searchFiles "
-                                class="px-6 py-4 bg-gradient-to-r from-blue-500 to-blue-600 text-white hover:from-blue-600 hover:to-blue-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2">
-                                <Search class="w-5 h-5" />
+                                <button @click="searchFiles" class="px-4 py-2 text-white bg-blue-600 rounded-r-full hover:bg-blue-700">
+                                <Search />
                             </button>
                         </div>
                         <div class="absolute inset-y-0 left-4 flex items-center pointer-events-none">
                             <Search class="w-5 h-5 text-gray-400" />
                         </div>
                     </div>
-
                     <div v-if=" loading " class="text-center py-10">
                         <div class="animate-spin rounded-full h-16 w-16  border-t-4 border-b-4 border-blue-500 mx-auto">
                         </div>
@@ -220,17 +210,22 @@ const flattenedFiles = computed( () => {
                                 <a href="#" @click.prevent="previewPdf( file.id )"
                                     class="inline-flex items-center text-blue-600 hover:text-blue-800 font-medium transition-colors duration-200">
                                     Previsualizar PDF
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1" viewBox="0 0 20 20"
-                                        fill="currentColor">
-                                        <path fill-rule="evenodd"
-                                            d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
-                                            clip-rule="evenodd" />
-                                    </svg>
                                 </a>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div v-if="pdfVisible" class="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center">
+                        <div class="relative w-3/4 h-3/4 bg-white rounded-lg overflow-hidden">
+                            <button @click="closePdfView" class="absolute top-2 right-2 text-red-500">
+                                <XCircle class="w-8 h-8" />
+                            </button>
+                            <iframe
+                                :src="pdfUrl + '#toolbar=0&navpanes=0&scrollbar=0'"
+                                class="w-full h-full">
+                            </iframe>
+                        </div>
+                    </div>
             </main>
         </div>
     </div>
@@ -241,5 +236,8 @@ const flattenedFiles = computed( () => {
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+}
+iframe {
+    border: none;
 }
 </style>
