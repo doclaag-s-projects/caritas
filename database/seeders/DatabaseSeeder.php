5438510809c7b107a05d067200a5b26665c7c575
Francisco Cifuentes <javiercajas90@gmail.com>
2024-10-30 00:47:13 -0600

    [ADD] SCRIPT_User_Rol_Relation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\UsuarioRol;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Crear el rol ADMIN y obtener su ID
+        $roleId = DB::table('roles')->insertGetId([
+            'nombre' => 'ADMIN',
+            'estado' => 1,
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Crear el usuario y obtener su ID
+        $userId = User::factory()->create([
+            'name' => 'TEST',
+            'email' => 'test@com',
+            'gender' => 'male', // o 'female' según corresponda
+            'password' => Hash::make('1234'), // Asegúrate de usar Hash::make para encriptar la contraseña
+            'Crear' => true,
+            'Eliminar' => true,
+            'Editar' => true,
+            'current_team_id' => null,
+            'profile_photo_path' => null,
+            'estado' => 1,
+        ])->id;
+
+        // Crear la relación en la tabla usuarios_roles
+        UsuarioRol::create([
+            'usuario_id' => $userId,
+            'rol_id' => $roleId,
         ]);
     }
 }
